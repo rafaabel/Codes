d479331ccc9f45a3bbf63e4ee0278b2491a31126
rafaabel <107432970+rafaabel@users.noreply.github.com>
2025-04-01 18:05:40 -0300

    Update SendMailExpiredApplicationsPostman.ps1

--- AAD/PowerShell/SendMailExpiredApplicationsPostman.ps1
+++ AAD/PowerShell/SendMailExpiredApplicationsPostman.ps1
@@ -164,11 +164,11 @@ $Payload = @{
     from            = @{ email = $EmailFrom }
     subject         = $Subject
     content         = @(@{ type = "text/html"; value = $Body })
-} | ConvertTo-Json -Depth 10 -Compress
+}
 
 # Send the email using Postman API
 $Headers = @{ Authorization = "Bearer $AccessToken" }
-$Response = Invoke-RestMethod -Uri $PostmanApiUrl -Method Post -Headers $Headers -Body $Payload -ContentType "application/json"
+$Response = Invoke-RestMethod -Uri $PostmanApiUrl -Method Post -Headers $Headers -Body $Payload -ContentType "text/plain"
 
 # Output Response
 if ($Response.StatusCode -eq 202) {
