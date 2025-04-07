4a162c93bf51b5a9dae23f4f579cfb9f1948bdd7
rafaabel <107432970+rafaabel@users.noreply.github.com>
2025-04-07 10:49:13 -0300

    Update SendMailEntraSyncSendGrid.ps1

--- AAD/PowerShell/SendMailEntraSyncSendGrid.ps1
+++ AAD/PowerShell/SendMailEntraSyncSendGrid.ps1
@@ -77,11 +77,13 @@ if ($status.SyncDuration.TotalHours -gt 1) {
         from = @{ email = $EmailFrom }
         subject = $Subject
         content = @(@{ type = "text/html"; value = $Body })
-    } | ConvertTo-Json -Depth 10 -Compress
+    }
+
+    $JsonPayload = $Payload | ConvertTo-Json -Depth 10 
 
     # Send email using SendGrid
     $Headers = @{ Authorization = "Bearer $securedSendGridApiKey" }
-    $Response = Invoke-RestMethod -Uri $SendGridApiUrl -Method Post -Headers $Headers -Body $Payload -ContentType "application/json"
+    $Response = Invoke-RestMethod -Uri $SendGridApiUrl -Method Post -Headers $Headers -Body $JsonPayload -ContentType "application/json"
 
     # Output response
     if ($Response.StatusCode -eq 202) {
